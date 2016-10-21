256f24399d9bc367a021cff4f41d833a0cf96704
Tom Scott <tomscott292@gmail.com>
2016-10-21 20:55:48 +0100

    Adds tests to EmployeeDAO

--- EmployeeDatabase/src/Controller.java
+++ EmployeeDatabase/src/Controller.java
@@ -1,8 +1,83 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Controller {
 
 	public static void main(String[] args) {
+		// Create database object
+		EmployeeDAO db = new EmployeeDAO();
+		Employee emp = new Employee();
+		
+		// Array list to store employees
+		ArrayList<Employee> employees;
 
+		// Test selectAllEmployees
+		employees = db.selectAllEmployees();
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+		
+		
+		// Test select employee by name
+		String empName = "Fred Bloggs";
+		System.out.println("\nSelecting employee : " + empName);
+		// Test select employee by name
+		try {
+			emp = db.selectEmployeeByName(empName);
+			System.out.println("SUCCESS: Employee " + empName + " selected.");
+		} catch (SQLException e2) {
+			System.out.println("FAILURE: Employee " + empName + " could not be selected.");
+			e2.printStackTrace();
+		}
+
+		// Update employee
+		emp.setId("4");
+		emp.setName("Jack Henderson");
+		emp.setGender('M');
+		emp.setDob("25-04-1965");
+		emp.setAddress("York");
+		emp.setPostcode("HG4 2RD");
+		emp.setNatInscNo("TG185469J");
+		emp.setTitle("Administrator");
+		emp.setStartDate("01-08-2010");
+		emp.setSalary("23000");
+		emp.setEmail("Jack@example.com");
+		
+		
+		String id = "4";
+		
+		// Test insert employee
+		if (db.insertEmployee(emp)) {
+			System.out.println("SUCCESS: Employee " + emp.getName() + " inserted.");
+		} else {
+			System.out.println("FAILURE: Employee " + emp.getName() + " insertion failed.");
+		}
+		
+		// Test deleteEmployeeAtId
+		if (db.deleteEmployeeById(emp, id)) {
+			System.out.println("SUCCESS: Employee " + emp.getName() + " at ID = " + id + " deleted.");
+		} else {
+			System.out.println("FAILURE: Employee " + emp.getName() + " at ID = " + id + " deletion failed.");
+		}
+		
+		id = "10";
+		
+		// Test insert employeeAtId
+		if (db.insertEmployeeAtID(emp, id)) {
+			System.out.println("SUCCESS: Employee " + emp.getName() + " inserted at ID=" + id);
+		} else {
+			System.out.println("FAILURE: Employee " + emp.getName() + " insertion failed at ID=" +id);
+		}
+		
+
+		
+		// Close database connection
+		try {
+			db.closeConnection();
+		} catch (SQLException e1) {
+			System.out.println("closeConnection sql exception");
+			e1.printStackTrace();
+		}
 	}
 
 }
