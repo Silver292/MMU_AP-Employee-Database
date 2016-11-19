591ef1bb08556bf943c8709c6a84e4b76a40b5e3
Tom Scott <tomscott292@gmail.com>
2016-11-19 15:16:17 +0000

    Refactors common code from DAO adds image storage and retrieval.

--- EmployeeDatabase/src/uk/tlscott/EmployeeDAO.java
+++ EmployeeDatabase/src/uk/tlscott/EmployeeDAO.java
@@ -3,7 +3,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -61,6 +63,57 @@ public class EmployeeDAO {
 		}
 	}
 	
+	public boolean insertEmployee(Employee emp) {
+		String sql = "INSERT INTO employees "
+				+ "(Name, Gender, DOB, Address, Postcode, NIN, JobTitle, StartDate, Salary, Email, Image) "
+				+ "VALUES "
+				+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"; 
+		try {
+			this.getConnection();
+			pstmt = c.prepareStatement(sql);
+	        setPreparedStatement(emp);
+			pstmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			LOGGER.log(Level.WARNING, "Error inserting employee", e);
+		} finally {
+			this.closeConnection();
+		}
+		return false;
+	}
+
+	//	TODO DO we need to pass string id?
+	public boolean updateEmployee(Employee emp, String id) {
+		String sql = "UPDATE employees SET "
+				+ "Name = ?, "
+	            + "Gender = ?, "
+	            + "DOB = ?, "
+	            + "Address = ?, "
+	            + "Postcode = ?, "
+	            + "NIN = ?, "
+	            + "JobTitle = ?, "
+	            + "StartDate = ?, "
+	            + "Salary = ?, "
+	            + "Email = ?, "
+	            + "Image = ? "
+	            + "WHERE ID = ?;";
+		// convert string id to an integer for use with database
+		int idAsInt = Integer.parseInt(id);
+		try {
+			this.getConnection();
+			pstmt = c.prepareStatement(sql);
+	        setPreparedStatement(emp);
+	        pstmt.setInt(12, idAsInt);
+			pstmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			LOGGER.log(Level.WARNING, "Error updating employee record", e);
+		} finally {
+			this.closeConnection();
+		}
+		return false;
+	}
+
 	public ArrayList<Employee> selectAllEmployees() {
 		ArrayList<Employee> employees = new ArrayList<Employee>();
 		try {
@@ -68,19 +121,7 @@ public class EmployeeDAO {
 			s = c.createStatement();
 			r = s.executeQuery("SELECT * FROM employees;");
 			while(r.next()) {
-				Employee emp = new Employee();
-				emp.setId(Integer.toString(r.getInt("ID")));
-				emp.setName(r.getString("Name"));
-				emp.setGender(r.getString("Gender").charAt(0));
-				emp.setDob(r.getString("DOB"));
-				emp.setAddress(r.getString("Address"));
-				emp.setPostcode(r.getString("Postcode"));
-				emp.setNatInscNo(r.getString("NIN"));
-				emp.setTitle(r.getString("JobTitle"));
-				emp.setStartDate(r.getString("StartDate"));
-				emp.setSalary(r.getString("Salary"));
-				emp.setEmail(r.getString("Email"));
-				employees.add(emp);
+				employees.add(setEmployeeFields());
 			}
 			
 			this.closeConnection();
@@ -104,18 +145,7 @@ public class EmployeeDAO {
 			r = pstmt.executeQuery();
 			
 			while(r.next()) {
-				emp = new Employee();
-				emp.setId(Integer.toString(r.getInt("ID")));
-				emp.setName(r.getString("Name"));
-				emp.setGender(r.getString("Gender").charAt(0));
-				emp.setDob(r.getString("DOB"));
-				emp.setAddress(r.getString("Address"));
-				emp.setPostcode(r.getString("Postcode"));
-				emp.setNatInscNo(r.getString("NIN"));
-				emp.setTitle(r.getString("JobTitle"));
-				emp.setStartDate(r.getString("Salary"));
-				emp.setSalary("StartDate");
-				emp.setEmail(r.getString("Email"));
+				emp = setEmployeeFields();
 			}
 		} catch (SQLException e) {
 			LOGGER.log(Level.WARNING, "Error selecting employee named: " + name, e);
@@ -137,18 +167,7 @@ public class EmployeeDAO {
 			r = pstmt.executeQuery();
 			
 			if (r.next()) {
-				emp = new Employee();
-				emp.setId(Integer.toString(r.getInt("ID")));
-				emp.setName(r.getString("Name"));
-				emp.setGender(r.getString("Gender").charAt(0));
-				emp.setDob(r.getString("DOB"));
-				emp.setAddress(r.getString("Address"));
-				emp.setPostcode(r.getString("Postcode"));
-				emp.setNatInscNo(r.getString("NIN"));
-				emp.setTitle(r.getString("JobTitle"));
-				emp.setStartDate(r.getString("StartDate"));
-				emp.setSalary(r.getString("Salary"));
-				emp.setEmail(r.getString("Email"));
+				emp = setEmployeeFields();
 			}
 		} catch (SQLException e) {
 			LOGGER.log(Level.WARNING, "Error selecting employee id: " + id, e);
@@ -158,67 +177,118 @@ public class EmployeeDAO {
 		return emp;
 	}
 
-	public boolean insertEmployee(Employee emp) {
-		String sql = "INSERT INTO employees "
-				+ "(Name, Gender, DOB, Address, Postcode, NIN, JobTitle, StartDate, Salary, Email, Image) "
-				+ "VALUES "
-				+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"; 
+	public Employee selectFirstEmployee() {
+		Employee emp = null;
+		String sql = "SELECT * FROM employees WHERE id = (SELECT MIN(ID) FROM employees);";
 		try {
 			this.getConnection();
-			pstmt = c.prepareStatement(sql);
-            pstmt.setString(1, emp.getName());
-            pstmt.setString(2, String.valueOf(emp.getGender()));
-            pstmt.setString(3, emp.getDob());
-            pstmt.setString(4, emp.getAddress());
-            pstmt.setString(5, emp.getPostcode());
-            pstmt.setString(6, emp.getNatInscNo());
-            pstmt.setString(7, emp.getTitle());
-            pstmt.setString(8, emp.getStartDate());
-            pstmt.setString(9, emp.getSalary());
-            pstmt.setString(10, emp.getEmail());
-            pstmt.setBytes(11, readFile(emp.getImageFile()));
-			pstmt.executeUpdate();
-			return true;
+			s = c.createStatement();
+			r = s.executeQuery(sql);
+			if(r.next()) {
+				emp = setEmployeeFields();
+			}
 		} catch (SQLException e) {
-			LOGGER.log(Level.WARNING, "Error inserting employee", e);
+			LOGGER.log(Level.WARNING, "Error selecting first employee", e);
+			return emp;
 		} finally {
 			this.closeConnection();
 		}
-		return false;
+		
+		return emp;
 	}
-	
-	// TODO add image insertion
-	public boolean insertEmployeeAtID(Employee emp, String id) {
-		String sql = "INSERT INTO employees "
-				+ "(Name, Gender, DOB, Address, Postcode, NIN, JobTitle, StartDate, Salary, Email, ID) "
-				+ "VALUES "
-				+ "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
-		// convert string id to an integer for use with database
+
+	/**
+	 * Selects next employee in table based on employee id.
+	 * <br>
+	 * If at the end of the table will wrap around and return the first employee in the table.
+	 * @param id of the current employee.
+	 * @return Employee next employee in the table or first employee if there are no ids higher.
+	 */
+	public Employee getNextEmployee(String id) {
 		int idAsInt = Integer.parseInt(id);
+	
+		// check if there are any records with an id higher than current.
+		String sql = "SELECT COUNT(*) AS count FROM employees WHERE ID > ?;";
+		
+		Employee emp = null;
 		try {
-			this.getConnection();
+			this.getConnection();		
 			pstmt = c.prepareStatement(sql);
-            pstmt.setString(1, emp.getName());
-            pstmt.setString(2, String.valueOf(emp.getGender()));
-            pstmt.setString(3, emp.getDob());
-            pstmt.setString(4, emp.getAddress());
-            pstmt.setString(5, emp.getPostcode());
-            pstmt.setString(6, emp.getNatInscNo());
-            pstmt.setString(7, emp.getTitle());
-            pstmt.setString(8, emp.getStartDate());
-            pstmt.setString(9, emp.getSalary());
-            pstmt.setString(10, emp.getEmail());
-            pstmt.setInt(11, idAsInt);
-			pstmt.executeUpdate();
-			return true;
+			pstmt.setInt(1, idAsInt);
+			r = pstmt.executeQuery();
+	
+			// if count > 0 get next employee
+			int count = r.getInt("count");
+			if (count > 0 ) {
+				sql = "SELECT * FROM employees WHERE ID > ? ORDER BY ID LIMIT 1;";
+				pstmt = c.prepareStatement(sql);
+				pstmt.setInt(1, idAsInt);
+				r = pstmt.executeQuery();
+			} else {
+				// otherwise wrap around
+				sql = "SELECT * FROM employees WHERE id = (SELECT MIN(ID) FROM employees);";
+				s = c.createStatement();
+				r = s.executeQuery(sql);
+			}
+	
+			// fill in employee information
+			if (r.next()) {
+				emp = setEmployeeFields();
+			}
 		} catch (SQLException e) {
-			LOGGER.log(Level.WARNING, "Error inserting employee at id: " + id, e);
-			return false;
+			LOGGER.log(Level.WARNING, "Error selecting employee id: " + id, e);
 		} finally {
 			this.closeConnection();
 		}
+		return emp;
 	}
+
+	/**
+	 * Selects previous employee in table based on employee id.
+	 * <br>
+	 * If at the beginning of the table will wrap around and return the last employee in the table.
+	 * @param id of the current employee.
+	 * @return Employee previous employee in the table or last employee if there are no lower ids.
+	 */
+	public Employee getPreviousEmployee(String id) {
+		int idAsInt = Integer.parseInt(id);
 	
+		// check if there are any records with an id lower than current.
+		String sql = "SELECT COUNT(*) AS count FROM employees WHERE ID < ?;";
+		
+		Employee emp = null;
+		try {
+			this.getConnection();		
+			pstmt = c.prepareStatement(sql);
+			pstmt.setInt(1, idAsInt);
+			r = pstmt.executeQuery();
+	
+			// if count > 0 get previous employee
+			int count = r.getInt("count");
+			if (count > 0 ) {
+				sql = "SELECT * FROM employees WHERE ID < ? ORDER BY ID DESC LIMIT 1;";
+				pstmt = c.prepareStatement(sql);
+				pstmt.setInt(1, idAsInt);
+				r = pstmt.executeQuery();
+			} else {
+				// otherwise wrap around
+				sql = "SELECT * FROM employees WHERE id = (SELECT MAX(ID) FROM employees);";
+				s = c.createStatement();
+				r = s.executeQuery(sql);
+			}
+	
+			// fill in employee information
+			if (r.next()) {
+				emp = setEmployeeFields();
+			}
+		} catch (SQLException e) {
+			LOGGER.log(Level.WARNING, "Error selecting employee id: " + id, e);
+		} finally {
+			this.closeConnection();
+		}
+		return emp;
+	}
+
 	public boolean deleteEmployeeById(String id) {
 		int idAsInt = Integer.parseInt(id);
 		String sql = "DELETE FROM employees WHERE ID = ?;";
@@ -237,153 +307,71 @@ public class EmployeeDAO {
 		}
 		
 	}
-
-	/**
-	 * Selects next employee in table based on employee id.
-	 * <br>
-	 * If at the end of the table will wrap around and return the first employee in the table.
-	 * @param id of the current employee.
-	 * @return Employee next employee in the table or first employee if there are no ids higher.
+	
+/**
+	 * @return
+	 * @throws SQLException
 	 */
-	public Employee getNextEmployee(String id) {
-		int idAsInt = Integer.parseInt(id);
-
-		// check if there are any records with an id higher than current.
-		String sql = "SELECT COUNT(*) AS count FROM employees WHERE ID > ?;";
-		
-		Employee emp = null;
-		try {
-			this.getConnection();		
-			pstmt = c.prepareStatement(sql);
-			pstmt.setInt(1, idAsInt);
-			r = pstmt.executeQuery();
-
-			// if count > 0 get next employee
-			int count = r.getInt("count");
-			if (count > 0 ) {
-				sql = "SELECT * FROM employees WHERE ID > ? ORDER BY ID LIMIT 1;";
-				pstmt = c.prepareStatement(sql);
-				pstmt.setInt(1, idAsInt);
-				r = pstmt.executeQuery();
-			} else {
-				// otherwise wrap around
-				sql = "SELECT * FROM employees WHERE id = (SELECT MIN(ID) FROM employees);";
-				s = c.createStatement();
-				r = s.executeQuery(sql);
-			}
-
-			// fill in employee information
-			if (r.next()) {
-				emp = new Employee();
-				emp.setId(Integer.toString(r.getInt("ID")));
-				emp.setName(r.getString("Name"));
-				emp.setGender(r.getString("Gender").charAt(0));
-				emp.setDob(r.getString("DOB"));
-				emp.setAddress(r.getString("Address"));
-				emp.setPostcode(r.getString("Postcode"));
-				emp.setNatInscNo(r.getString("NIN"));
-				emp.setTitle(r.getString("JobTitle"));
-				emp.setStartDate(r.getString("StartDate"));
-				emp.setSalary(r.getString("Salary"));
-				emp.setEmail(r.getString("Email"));
-			}
-		} catch (SQLException e) {
-			LOGGER.log(Level.WARNING, "Error selecting employee id: " + id, e);
-		} finally {
-			this.closeConnection();
-		}
-		return emp;
-	}
-	
-	
-	public Employee selectFirstEmployee() {
-		Employee emp = null;
-		String sql = "SELECT * FROM employees WHERE id = (SELECT MIN(ID) FROM employees);";
-		try {
-			this.getConnection();
-			s = c.createStatement();
-			r = s.executeQuery(sql);
-			if(r.next()) {
-				emp = new Employee();
-				emp.setId(Integer.toString(r.getInt("ID")));
-				emp.setName(r.getString("Name"));
-				emp.setGender(r.getString("Gender").charAt(0));
-				emp.setDob(r.getString("DOB"));
-				emp.setAddress(r.getString("Address"));
-				emp.setPostcode(r.getString("Postcode"));
-				emp.setNatInscNo(r.getString("NIN"));
-				emp.setTitle(r.getString("JobTitle"));
-				emp.setStartDate(r.getString("StartDate"));
-				emp.setSalary(r.getString("Salary"));
-				emp.setEmail(r.getString("Email"));
-			}
-		} catch (SQLException e) {
-			LOGGER.log(Level.WARNING, "Error selecting first employee", e);
-			return emp;
-		} finally {
-			this.closeConnection();
-		}
-		
+	private Employee setEmployeeFields() throws SQLException {
+		Employee emp = new Employee();
+		emp.setId(Integer.toString(r.getInt("ID")));
+		emp.setName(r.getString("Name"));
+		emp.setGender(r.getString("Gender").charAt(0));
+		emp.setDob(r.getString("DOB"));
+		emp.setAddress(r.getString("Address"));
+		emp.setPostcode(r.getString("Postcode"));
+		emp.setNatInscNo(r.getString("NIN"));
+		emp.setTitle(r.getString("JobTitle"));
+		emp.setStartDate(r.getString("StartDate"));
+		emp.setSalary(r.getString("Salary"));
+		emp.setEmail(r.getString("Email"));
+		emp.setImage(getImageFile(emp));
 		return emp;
 	}
 
 	/**
- 	 * Selects previous employee in table based on employee id.
-	 * <br>
-	 * If at the beginning of the table will wrap around and return the last employee in the table.
-	 * @param id of the current employee.
-	 * @return Employee previous employee in the table or last employee if there are no lower ids.
+	 * @param emp
+	 * @throws SQLException
 	 */
-	public Employee getPreviousEmployee(String id) {
-		int idAsInt = Integer.parseInt(id);
-
-		// check if there are any records with an id lower than current.
-		String sql = "SELECT COUNT(*) AS count FROM employees WHERE ID < ?;";
-		
-		Employee emp = null;
-		try {
-			this.getConnection();		
-			pstmt = c.prepareStatement(sql);
-			pstmt.setInt(1, idAsInt);
-			r = pstmt.executeQuery();
-
-			// if count > 0 get previous employee
-			int count = r.getInt("count");
-			if (count > 0 ) {
-				sql = "SELECT * FROM employees WHERE ID < ? ORDER BY ID DESC LIMIT 1;";
-				pstmt = c.prepareStatement(sql);
-				pstmt.setInt(1, idAsInt);
-				r = pstmt.executeQuery();
-			} else {
-				// otherwise wrap around
-				sql = "SELECT * FROM employees WHERE id = (SELECT MAX(ID) FROM employees);";
-				s = c.createStatement();
-				r = s.executeQuery(sql);
-			}
-
-			// fill in employee information
-			if (r.next()) {
-				emp = new Employee();
-				emp.setId(Integer.toString(r.getInt("ID")));
-				emp.setName(r.getString("Name"));
-				emp.setGender(r.getString("Gender").charAt(0));
-				emp.setDob(r.getString("DOB"));
-				emp.setAddress(r.getString("Address"));
-				emp.setPostcode(r.getString("Postcode"));
-				emp.setNatInscNo(r.getString("NIN"));
-				emp.setTitle(r.getString("JobTitle"));
-				emp.setStartDate(r.getString("StartDate"));
-				emp.setSalary(r.getString("Salary"));
-				emp.setEmail(r.getString("Email"));
-			}
-		} catch (SQLException e) {
-			LOGGER.log(Level.WARNING, "Error selecting employee id: " + id, e);
-		} finally {
-			this.closeConnection();
-		}
-		return emp;
+	private void setPreparedStatement(Employee emp) throws SQLException {
+		pstmt.setString(1, emp.getName());
+		pstmt.setString(2, String.valueOf(emp.getGender()));
+		pstmt.setString(3, emp.getDob());
+		pstmt.setString(4, emp.getAddress());
+		pstmt.setString(5, emp.getPostcode());
+		pstmt.setString(6, emp.getNatInscNo());
+		pstmt.setString(7, emp.getTitle());
+		pstmt.setString(8, emp.getStartDate());
+		pstmt.setString(9, emp.getSalary());
+		pstmt.setString(10, emp.getEmail());
+        pstmt.setBytes(11, readFile(emp.getImageFile()));
 	}
+
+	/**
+	 * @param emp
+	 * @throws SQLException
+	 */
+	private File getImageFile(Employee emp) throws SQLException {
+		File file = new File("./assets/empImage" + emp.getId() + ".jpg");
+		
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			InputStream input = r.getBinaryStream("Image");
+			if(input != null) {
+				// write to file from database
+				byte[] buffer = new byte[1024];
+				while (input.read(buffer) > 0) {
+					fos.write(buffer);
+				}
+				return file;
+			} 
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Error creating tempory image file", e);
+		}
 	
+		return null;
+	}
+
 	//TODO: Write our own
 	/**
      * Read the file and returns the byte array
